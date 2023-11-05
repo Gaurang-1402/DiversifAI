e08f878201e04f2868ab03e816eb42ee67b1d8fb
avocadodip <56790819+avocadodip@users.noreply.github.com>
2023-11-04 20:31:37 -0500

    google sign in page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -12,13 +12,20 @@ export default function AuthStart() {
     return (
         <div>
             <BottomNavbar user={user} />
-            <div className='px-20 my-20 flex flex-col gap-10 max-w-xs'>
-                <Link href={LOGIN}>
-                    <ButtonPrimarySmall>
-                        Login
-                    </ButtonPrimarySmall>
-                </Link>
-                <Link href={REGISTER_AS_RECRUITER_SCREEN}>
+            <div className='px-8 mt-20 flex flex-col max-w-xs'>
+            <h1 className="text-[#8000FF] font-bold text-4xl">Diversif<span className="text-[#18C7D2]">AI</span></h1>
+            <h2 className="text-gray-500 font-medium text-2xl mt-2">Register</h2>
+            
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <Link href={LOGIN}>
+                        <button className="flex items-center justify-center gap-8 py-3 rounded-[12px] bg-white px-6 mt-4 border-4 border-[#8000FF]">
+                            <p className="text-[#8000FF] font-bold text-lg">Sign in with Google</p>
+                            <img src="/google.png" alt="search" width={30} height={30} className="" />
+  
+                        </button>
+                    </Link>
+                </div>
+                {/* <Link href={REGISTER_AS_RECRUITER_SCREEN}>
                     <ButtonPrimarySmall>
                         Register as Recuiter
                     </ButtonPrimarySmall>
@@ -27,7 +34,7 @@ export default function AuthStart() {
                     <ButtonPrimarySmall>
                         Register as Student
                     </ButtonPrimarySmall>
-                </Link>
+                </Link> */}
             </div>
         </div>
 
